066129bad5ad79ac91796331a56bd94af90a5c7e
Andrey Zhidkih <andry.zhidkikh@gmail.com>
2017-04-06 23:58:30 +0300

    lesson 2

---
+++ lesson one/Lab 2.2/lab_2_2.js
@@ -0,0 +1,8 @@
+﻿var num=2
+var exp=10;
+var result=1;
+;
+for(var cnt=0 ;cnt<exp; cnt++){
+	result*=num;
+}
+console.log(result);
---
+++ lesson one/Lab1/lab_2_1.js
@@ -0,0 +1,23 @@
+﻿// Лабораторная номер 1 Возведение числа 2 в степень 10
+/*var num=2;
+var exp=10;
+var result=1;// 
+var cnt=0;
+while(cnt < exp){
+	result*=num;// Умножаем и присваеваем
+	cnt++;
+};
+console.log(result);
+*/
+// триугольник из звездочек
+/*var lines=6;
+var symbol="*";
+result="";
+var cnt=0;
+while(cnt<=lines){
+ 	result+=symbol;
+ 	cnt++;
+ 	console.log(result);
+};
+ /*
+ 
