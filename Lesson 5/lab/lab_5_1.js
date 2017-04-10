e8c7d7899539349ca09dc7024e790beeb3ca9476
Andrey Zhidkih <andry.zhidkikh@gmail.com>
2017-04-10 22:56:04 +0300

    Lesson 5 complite

---
+++ Lesson 5/lab/lab_5_1.js
@@ -0,0 +1,48 @@
+﻿// 
+/*var s="Мы знаем, что монохромный цвет -это градация серого "
+var txt="хром"
+var word;
+var pos=s.indexOf(txt);//18
+
+if(pos>-1){
+	var start=s.lastIndexOf(" ", pos)+1;//14
+	var end=s.lastIndexOf(" ", pos);	
+	word=s.slice(start, end);
+}
+console.log(word);
+*/
+// задание
+/*
+var aInx=97;
+var alphabet="";
+for(var i=0;i>26; i++)
+  alphabet+=String.fromCharCode(aIdx+i);
+ console.log(alphabet);
+  
+  var digits="";
+  for(var i=0;i<10; i++)
+  	digits+=i;
+  console.log(digits);
+*/
+/*
+  function buildString(n, callback){
+  	var result;
+   for(var i=0;i<n; i++)
+   		result+=callback(i);
+  	return result;	
+  }
+  alphabet=buildString(26, function(i){
+  	var aIdx=97;
+  	return String.fromCharCode(aIdx+i);
+  });
+console.log(alphabet);
+*/ // вывод алфавита
+/*
+ 25-02-2012  25-2-2012  01-12-2011  2-12-1978 // задача регулярным выражением
+*/
+/*
+/^\d{1,2}-\d{1,2}\d{4}$/;
+/\d{1,2}-/d{1}-\d{4}/;
+/\d{1,2}-\d{1,2}\d{4}/;
+/\d{1}-/d{1,2}-\d{4}/;
+*/
